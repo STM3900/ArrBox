90cffb19f593ede802167334463d1e981d0e45f5
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2022-06-12 18:35:06 +0200

    v1 fini

--- src/main.cpp
+++ src/main.cpp
@@ -84,7 +84,7 @@ void setup()
   Serial.println(myDFPlayer.readFileCounts());        // Le nombre total de fichier mp3 sur la carte ( dossier inclus )
   Serial.println(myDFPlayer.readCurrentFileNumber()); // l'index courant
 
-  maxIndex = myDFPlayer.readFileCounts() + 1; 
+  maxIndex = myDFPlayer.readFileCounts() + 1;
 
   // Connection au wifi
   setRGBLight(0, 0, 255);
@@ -186,18 +186,98 @@ void loop()
     client.println("");
     client.println("<!DOCTYPE HTML>");
     client.println("<html>");
+    client.println("<head>");
+    client.println("<meta charset=\"UTF-8\" />");
+    client.println("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />");
+    client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />");
+    client.println("<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\" />");
+    client.println("<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin />");
+    client.println("<link href=\"https://fonts.googleapis.com/css2?family=Fredericka+the+Great&display=swap\" rel=\"stylesheet\"/>");
+    client.println("<title>ARR</title>");
+    client.println("<style>");
+    client.println("body {");
+    client.println("font-family: \"Fredericka the Great\", cursive;");
+    client.println("margin: 0;");
+    client.println("width: 100%;");
+    client.println("height: 100vh;");
+    client.println("display: flex;");
+    client.println("flex-direction: column;");
+    client.println("justify-content: center;");
+    client.println("align-items: center;");
+    client.println("align-content: center;");
+    client.println("background-image: url(https://cdn.discordapp.com/attachments/455791465734602782/985571028841431151/motif.png);");
+    client.println("background-repeat: repeat;");
+    client.println("}");
+    client.println("h1 {");
+    client.println("font-size: 120px;");
+    client.println("font-weight: normal;");
+    client.println("margin-top: 0;");
+    client.println("color: rgb(78, 67, 56);");
+    client.println("}");
+    client.println("button {");
+    client.println("font-family: \"Fredericka the Great\", cursive;");
+    client.println("font-size: 40px;");
+    client.println("padding: 10px 40px;");
+    client.println("border: solid rgb(80, 69, 58) 3px;");
+    client.println("background: none;");
+    client.println("color: rgb(58, 50, 41);");
+    client.println("font-weight: bold;");
+    client.println("transform: rotate(-4deg);");
+    client.println("transition: 0.3s;");
+    client.println("}");
+    client.println("button:hover {");
+    client.println("cursor: pointer;");
+    client.println("transform: scale(0.99) rotate(-5deg);");
+    client.println("}");
+    client.println("button:active {");
+    client.println("transform: scale(0.97) rotate(-2deg);");
+    client.println("}");
+    client.println("div {");
+    client.println("position: fixed;");
+    client.println("width: 100%;");
+    client.println("height: 100%;");
+    client.println("display: flex;");
+    client.println("flex-direction: column;");
+    client.println("justify-content: center;");
+    client.println("align-items: center;");
+    client.println("align-content: center;");
+    client.println("background-image: url(https://cdn.discordapp.com/attachments/455791465734602782/985571029038534716/ARR.png);");
+    client.println("background-repeat: no-repeat;");
+    client.println("background-size: contain;");
+    client.println("background-position: center;");
+    client.println("filter: sepia(0.3);");
+    client.println("opacity: 0.3;");
+    client.println("z-index: -1;");
+    client.println("}");
+    client.println("@media screen and (max-width: 480px) {");
+    client.println("h1 {");
+    client.println("font-size: 80px;");
+    client.println("}");
+    client.println("button {");
+    client.println("border: solid rgb(80, 69, 58) 2px;");
+    client.println("font-size: 32px;");
+    client.println("}");
+    client.println("div {");
+    client.println("background-size: cover;");
+    client.println("}");
+    client.println("}");
+    client.println("</style>");
+    client.println("</head>");
+    client.println("<body>");
     client.println("<h1>Arr ?</h1>");
-    client.println("<br><br>");
+    client.println("<div></div>");
+
     if (value == HIGH)
     {
       // client.print("On");
-      client.println("<a href=\"/BTN=OFF\"\"><button>ARR </button></a>");
+      client.println("<a href=\"/BTN=OFF\"\"><button>ARR</button></a>");
     }
     else
     {
       // client.print("Off");
-      client.println("<a href=\"/BTN=ON\"\"><button>ARR </button></a>");
+      client.println("<a href=\"/BTN=ON\"\"><button>ARR</button></a>");
     }
+    client.println("</body>");
     client.println("</html>");
 
     delay(1);
